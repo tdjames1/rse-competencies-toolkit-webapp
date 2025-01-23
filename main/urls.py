d9e83d515da4ffa2bcdace9d5c35b590061ef03e
Adrian D'Alessandro <a.dalessandro@imperial.ac.uk>
2025-01-23 18:08:12 +0000

    Add contrib.auth urls to the main app urls

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,11 @@
 """Urls module for the main app."""
 
-from django.urls import path
+from django.urls import include, path
 
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
     path("privacy/", views.privacy, name="privacy"),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
