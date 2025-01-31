07a95ed3e51d2200b192345c17f1fa9db070a95b
Adrian D'Alessandro <a.dalessandro@imperial.ac.uk>
2025-01-31 16:07:55 +0000

    Make category model

--- main/models.py
+++ main/models.py
@@ -8,6 +8,16 @@ class User(AbstractUser):
     """Custom user model for this project."""
 
 
+class Category(models.Model):
+    """Model for categories."""
+
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    parent_category = models.ForeignKey(
+        "self", on_delete=models.CASCADE, null=True, blank=True
+    )
+
+
 class SkillLevel(models.Model):
     """Model for skill levels."""
 
