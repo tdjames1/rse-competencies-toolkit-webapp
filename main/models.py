75ce1f4fd1fe4416453c88f5da4576c429cd925c
Adrian D'Alessandro <a.dalessandro@imperial.ac.uk>
2025-01-31 17:27:08 +0000

    Add some validation to category model

--- main/models.py
+++ main/models.py
@@ -1,7 +1,9 @@
 """Models module for the main app."""
 
 from django.contrib.auth.models import AbstractUser
+from django.core.exceptions import ValidationError
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class User(AbstractUser):
@@ -14,8 +16,31 @@ class Category(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=200)
     parent_category = models.ForeignKey(
-        "self", on_delete=models.CASCADE, null=True, blank=True
+        "self",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        limit_choices_to={"parent_category": None},
     )
+
+    def __str__(self) -> str:
+        """Return the name of the category."""
+        return self.name
+
+    def clean(self) -> None:
+        """Validate the category instance."""
+        if self.parent_category == self:
+            raise ValidationError(
+                {"parent_category": _("A category cannot be its own parent.")}
+            )
+        if self.parent_category and self.category_set.all().exists():
+            raise ValidationError(
+                {
+                    "parent_category": _(
+                        "This is a parent category so can't be made into a subcategory."
+                    )
+                }
+            )
 
 
 class SkillLevel(models.Model):
