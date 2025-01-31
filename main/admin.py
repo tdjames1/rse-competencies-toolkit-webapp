388257a6bdf630cd516462a35ed726d301b35978
Adrian D'Alessandro <a.dalessandro@imperial.ac.uk>
2025-01-31 17:37:33 +0000

    Add SkillLevel to admin
    Requires monkeypatching with django-stubs-ext

--- main/admin.py
+++ main/admin.py
@@ -3,6 +3,15 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import User
+from .models import SkillLevel, User
 
 admin.site.register(User, UserAdmin)
+
+
+@admin.register(SkillLevel)
+class SkillLevelAdmin(admin.ModelAdmin[SkillLevel]):
+    """Admin class for skill levels."""
+
+    list_display = ("name",)
+    search_fields = ("name",)
+    ordering = ("name",)
--- rse_competencies_toolkit/settings/__init__.py
+++ rse_competencies_toolkit/settings/__init__.py
@@ -1,3 +1,7 @@
 """Django settings for rse_competencies_toolkit project."""
 
+import django_stubs_ext
+
 from .settings import *  # noqa: F403
+
+django_stubs_ext.monkeypatch()
