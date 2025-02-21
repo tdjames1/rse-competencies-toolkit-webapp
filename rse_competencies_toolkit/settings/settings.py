a114b6beedcbd1ec374ddb7ff88c0f77bc73ef6b
Connor Aird <c.aird@ucl.ac.uk>
2025-02-21 16:55:01 +0000

    Add login and password reset view

--- rse_competencies_toolkit/settings/settings.py
+++ rse_competencies_toolkit/settings/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS: list[str]
 # Application definition
 
 INSTALLED_APPS = [
+    "main",  # main must go before the defaults to override the default admin views
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -121,10 +122,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-
-# Custom settings
-INSTALLED_APPS += ["main"]
 
 
 MIDDLEWARE.insert(1, "whitenoise.middleware.WhiteNoiseMiddleware")
