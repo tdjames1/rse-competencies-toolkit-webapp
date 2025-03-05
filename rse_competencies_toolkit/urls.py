694ffa9a7c344c98944bb121f69217d06c10d5af
Tamora D. James <t.d.james@sheffield.ac.uk>
2025-03-05 18:01:23 +0000

    Remove unnecessary login url pattern
    Noting that #137 added contrib.auth urls to the main app urls, providing the necessary urls for registration actions.

--- rse_competencies_toolkit/urls.py
+++ rse_competencies_toolkit/urls.py
@@ -16,11 +16,9 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
 from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("login", auth_views.LoginView.as_view(), name="login"),
     path("", include("main.urls")),
 ]
