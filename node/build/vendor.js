1bde1e0e3fce9ab51b6c406491e31aa2d48376c4
Dave Horsfall <dave@horsfall.dev>
2025-03-11 12:34:03 +0000

    bundle vendor js

--- node/build/vendor.js
+++ node/build/vendor.js
@@ -1,4 +1,8 @@
 const path = require('./config')
+const rollup = require('rollup')
+const nodeResolve = require('@rollup/plugin-node-resolve')
+const commonjs = require('@rollup/plugin-commonjs') // Fix CommonJS issues
+const terser = require('@rollup/plugin-terser')
 const fs = require('fs-extra')
 const packageFile = require('../../package.json')
 const configureLogger = require('./logger')
@@ -6,6 +10,18 @@ const configureLogger = require('./logger')
 const log = configureLogger('Vendor')
 
 const excludedDependencies = ['bootstrap', 'smooth-scroll']
+const vendorFile = `${path.js}/vendor.bundle.js`
+
+const getVendorEntries = () => {
+  const dependencies = Object.keys(packageFile.dependencies).filter(
+    (dependency) => !excludedDependencies.includes(dependency)
+  )
+
+  return (
+    dependencies.map((dep) => `import '${dep}'`).join('\n') +
+    "\nimport AOS from 'aos'\nwindow.AOS = AOS" // Expose AOS globally
+  )
+}
 
 const cleanVendorDirectory = async () => {
   log.info('Cleaning vendor directory...')
@@ -17,30 +33,36 @@ const cleanVendorDirectory = async () => {
   }
 }
 
-const copyDependencies = async () => {
-  log.info('Copying dependencies...')
-  const dependencies = Object.keys(packageFile.dependencies).filter(
-    (dependency) => !excludedDependencies.includes(dependency)
-  )
+const bundleVendorScripts = async () => {
+  log.info('Bundling vendor scripts...')
+  try {
+    const vendorEntry = `${path.src_js}/vendor.js`
 
-  let errors = 0
-  for (const dependency of dependencies) {
-    try {
-      await fs.copy(`node_modules/${dependency}`, `${path.vendor}/${dependency}`)
-    } catch (error) {
-      errors++
-      log.error('', `Failed to copy ${dependency}: ${error.message}`)
-    }
-  }
+    // Generate a temporary entry file
+    await fs.writeFile(vendorEntry, getVendorEntries())
 
-  if (errors === 0) {
-    log.success('All dependencies copied successfully')
-  } else {
-    log.error('', `${errors} dependencies failed to copy`)
+    const bundle = await rollup.rollup({
+      input: vendorEntry,
+      plugins: [
+        nodeResolve({ browser: true, preferBuiltins: false }), // Fix module resolution
+        commonjs({ include: /node_modules/ }), // Handle CommonJS modules properly
+        terser(), // Minify output
+      ],
+    })
+
+    await bundle.write({
+      file: vendorFile,
+      format: 'iife',
+      sourcemap: true,
+    })
+
+    log.success('Bundled vendor scripts successfully')
+  } catch (error) {
+    log.error('', `Failed to bundle vendor scripts: ${error.message}`)
   }
 }
 
 ;(async () => {
   await cleanVendorDirectory()
-  await copyDependencies()
+  await bundleVendorScripts()
 })()
