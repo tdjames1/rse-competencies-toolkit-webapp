480f2e5bcb4128c34a431866a3922919f8a3fe4c
Adrian D'Alessandro <a.dalessandro@imperial.ac.uk>
2025-03-28 15:11:34 +0000

    Convert main views tests to pytest-style

--- tests/main/test_main_views.py
+++ tests/main/test_main_views.py
@@ -1,24 +1,24 @@
-# from django.core import mail
+"""Test suite for the main views.
 
-from django.test import TestCase
+This test module includes tests for main views of the app ensuring that:
+  - The correct templates are used.
+  - The correct status codes are returned.
+"""
+
+from http import HTTPStatus
+
 from pytest_django.asserts import assertTemplateUsed
 
 
-class TestMainViews(TestCase):
-    """Test suite for the main views of the RSE Competencies Toolkit web application.
+def test_index(client):
+    """Test the index view is routed to the correct template."""
+    with assertTemplateUsed(template_name="main/index.html"):
+        response = client.get("/")
+    assert response.status_code == HTTPStatus.OK
 
-    This test case includes tests for main views of the app ensurign that the correct
-    templates are used
-    """
 
-    def test_index(self):
-        """Test the index view."""
-        with assertTemplateUsed(template_name="main/index.html"):
-            response = self.client.get("/")
-        assert response.status_code == 200
-
-    def test_privacy(self):
-        """Test the privacy page view."""
-        with assertTemplateUsed(template_name="main/privacy.html"):
-            response = self.client.get("/privacy/")
-        self.assertEqual(response.status_code, 200)
+def test_privacy(client):
+    """Test the privacy page view is routed to the correct template."""
+    with assertTemplateUsed(template_name="main/privacy.html"):
+        response = client.get("/privacy/")
+    assert response.status_code == HTTPStatus.OK
