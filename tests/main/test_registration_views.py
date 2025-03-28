2b6c6a7aeff9b51757d6312971f43f6d4d40e03c
Adrian D'Alessandro <a.dalessandro@imperial.ac.uk>
2025-03-28 15:44:26 +0000

    Convert test_registration_views to pytest style

--- tests/main/test_registration_views.py
+++ tests/main/test_registration_views.py
@@ -1,63 +1,65 @@
-# from django.core import mail
+"""Test suite for the registration views."""
+
+from http import HTTPStatus
+
+import pytest
 from django.core import mail
-from django.test import TestCase
 from django.urls import reverse
-from pytest_django.asserts import assertTemplateUsed
-
-from main.models import User
+from pytest_django.asserts import assertContains, assertNotContains, assertTemplateUsed
 
 
-class TestRegistrationViews(TestCase):
-    """Test suite for the registration views.
+@pytest.fixture
+def password_reset_uid_and_token(client, user):
+    """Returns the uid and token for password reset."""
+    response = client.post(reverse("password_reset"), data={"email": user.email})
+    token = response.context[0]["token"]
+    uid = response.context[0]["uid"]
 
-    This test case includes tests for the password reset functionality,
-    ensuring that the password reset form is rendered correctly and that
-    the password reset submission process works as expected, including
-    email generation and redirection.
-    """
+    return uid, token
 
-    def setUp(self):
-        """Set up the test environment with a test user with predefined attributes.
 
-        This is required to allow the email generation test to work.
-        """
-        self.test_user = User.objects.create_user(
-            first_name="test",
-            last_name="user",
-            email="test.user@mail.com",
-            password="1234",
-            username="testuser",
-        )
+@pytest.fixture
+def password_reset_url(password_reset_uid_and_token):
+    """Returns the password reset URL."""
+    uid, token = password_reset_uid_and_token
+    return reverse("password_reset_confirm", kwargs={"uidb64": uid, "token": token})
 
-    def test_password_reset(self):
-        """Test the password_reset_form view."""
+
+@pytest.fixture
+def set_password_url(client, password_reset_uid_and_token, password_reset_url):
+    """Returns the set password URL."""
+    uid, _ = password_reset_uid_and_token
+    client.get(password_reset_url)
+    return reverse(
+        "password_reset_confirm", kwargs={"uidb64": uid, "token": "set-password"}
+    )
+
+
+class TestPasswordReset:
+    """Test suite for the password_reset views."""
+
+    def _get_url(self):
+        return reverse("password_reset")
+
+    def test_get(self, client):
+        """Test the view GET request uses the correct template."""
         with assertTemplateUsed(template_name="registration/password_reset_form.html"):
-            response = self.client.get("/accounts/password_reset/")
-        self.assertEqual(response.status_code, 200)
+            response = client.get(self._get_url())
+        assert response.status_code == HTTPStatus.OK
 
-    def test_password_reset_done(self):
-        """Test the password_reset_done view."""
-        with assertTemplateUsed(template_name="registration/password_reset_done.html"):
-            response = self.client.get("/accounts/password_reset/done/")
-        self.assertEqual(response.status_code, 200)
-
-    def test_password_reset_email_and_subject(self):
-        """Test the password reset email and subject templates."""
+    def test_post(self, client, user):
+        """Test the view POST request redirects correctly."""
         # Request a password reset email
-        response = self.client.post(
-            path="/accounts/password_reset/", data={"email": self.test_user.email}
-        )
+        response = client.post(self._get_url(), data={"email": user.email})
+
         # Assert redirects to password_reset/done
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response.url, "/accounts/password_reset/done/")
+        assert response.status_code == HTTPStatus.FOUND
+        assert response.url == reverse("password_reset_done")
 
-        # Get the token and userid from the response
-        token = response.context[0]["token"]
-        uid = response.context[0]["uid"]
-        generated_reset_url = reverse(
-            "password_reset_confirm", kwargs={"token": token, "uidb64": uid}
-        )
-
+    def test_password_reset_email_and_subject(
+        self, client, user, password_reset_url, set_password_url
+    ):
+        """Test the password reset email and subject templates."""
         # Verify the generated email against the expected templates
         with open("main/templates/registration/password_reset_subject.txt") as f:
             expected_email_subject = f.read()
@@ -65,7 +67,7 @@ class TestRegistrationViews(TestCase):
             expected_email_content = f.read()
             # Replace template variables
             expected_email_content = expected_email_content.replace(
-                "{{ email }}", self.test_user.email
+                "{{ email }}", user.email
             )
             expected_email_content = expected_email_content.replace(
                 "{{ protocol }}", "http"
@@ -75,68 +77,82 @@ class TestRegistrationViews(TestCase):
             )
             expected_email_content = expected_email_content.replace(
                 "{% url 'password_reset_confirm' uidb64=uid token=token %}",
-                generated_reset_url,
+                password_reset_url,
             )
 
-        self.assertEqual(len(mail.outbox), 1)
+        assert len(mail.outbox) == 1
         generated_email = mail.outbox[0]
-        self.assertEqual(generated_email.subject, expected_email_subject.strip())
-        self.assertEqual(generated_email.body.strip(), expected_email_content.strip())
+        assert generated_email.subject, expected_email_subject.strip()
+        assert generated_email.body.strip(), expected_email_content.strip()
 
         # Now we can use the token to get the password change form
-        set_password_url = f"/accounts/reset/{uid}/set-password/"
-        response = self.client.get(generated_reset_url)
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response.url, set_password_url)
+        response = client.get(password_reset_url)
+        assert response.status_code == HTTPStatus.FOUND
+        assert response.url == set_password_url
 
-        return set_password_url
 
-    def test_password_reset_confirm(self):
-        """Test the password_reset_confirm view."""
-        set_password_url = self.test_password_reset_email_and_subject()
+class TestPasswordResetDone:
+    """Test suite for the password_reset_done views."""
 
+    def test_password_reset_done(self, client):
+        """Test the password_reset_done view."""
+        with assertTemplateUsed(template_name="registration/password_reset_done.html"):
+            response = client.get("/accounts/password_reset/done/")
+        assert response.status_code == HTTPStatus.OK
+
+
+class TestPasswordResetConfirm:
+    """Test suite for the password_reset_confirm views."""
+
+    def test_get(self, client, set_password_url):
+        """Test the view GET request uses the correct template."""
         # Expect out custom error page if an incorrect token/uid is used
         with assertTemplateUsed(
             template_name="registration/password_reset_confirm.html"
         ):
-            response = self.client.get(
+            response = client.get(
                 reverse(
                     "password_reset_confirm",
                     kwargs={"token": "some", "uidb64": "thing"},
                 )
             )
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "Password reset failed")
+        assert response.status_code == HTTPStatus.OK
+        assertContains(response, "Password reset failed")
 
         # Expect our custom password_reset_confirm page
         with assertTemplateUsed(
             template_name="registration/password_reset_confirm.html"
         ):
-            response = self.client.get(set_password_url)
-        self.assertEqual(response.status_code, 200)
-        self.assertNotContains(response, "Password reset failed")
+            response = client.get(set_password_url)
+        assert response.status_code == HTTPStatus.OK
+        assertNotContains(response, "Password reset failed")
 
-    def test_password_reset_complete(self):
+
+class TestPasswordResetComplete:
+    """Test suite for the password_reset_complete views."""
+
+    def _get_url(self):
+        return reverse("password_reset_complete")
+
+    def test_get(self, client):
+        """Test the view GET request uses the correct template."""
+        # Expect our custom password_reset_complete page
+        with assertTemplateUsed(
+            template_name="registration/password_reset_complete.html"
+        ):
+            response = client.get(self._get_url())
+        assert response.status_code == HTTPStatus.OK
+
+    def test_password_reset_complete(self, client, set_password_url):
         """Test the password_reset_complete view."""
-        set_password_url = self.test_password_reset_email_and_subject()
-
-        expected_redirect_url = "/accounts/reset/done/"
-
         # Set the new password
         # Note that this needs to be sufficiently strong to be accepted
-        response = self.client.post(
+        response = client.post(
             set_password_url,
             data={
                 "new_password1": "bkjbkjwdnwqkldnwkjfdnqlkecf",
                 "new_password2": "bkjbkjwdnwqkldnwkjfdnqlkecf",
             },
         )
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response.url, expected_redirect_url)
-
-        # Expect our custom password_reset_complete page
-        with assertTemplateUsed(
-            template_name="registration/password_reset_complete.html"
-        ):
-            response = self.client.get(expected_redirect_url)
-        self.assertEqual(response.status_code, 200)
+        assert response.status_code == HTTPStatus.FOUND
+        assert response.url == self._get_url()
