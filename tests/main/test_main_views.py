cc06642908e42af2e9d8d89ed9146eacd13a410d
Adrian D'Alessandro <a.dalessandro@imperial.ac.uk>
2025-03-28 15:56:27 +0000

    Include example view test using loginmixin, marked xfail

--- tests/main/test_main_views.py
+++ tests/main/test_main_views.py
@@ -7,7 +7,11 @@ This test module includes tests for main views of the app ensuring that:
 
 from http import HTTPStatus
 
+import pytest
+from django.urls import reverse
 from pytest_django.asserts import assertTemplateUsed
+
+from .view_utils import LoginRequiredMixin
 
 
 def test_index(client):
@@ -22,3 +26,17 @@ def test_privacy(client):
     with assertTemplateUsed(template_name="main/privacy.html"):
         response = client.get("/privacy/")
     assert response.status_code == HTTPStatus.OK
+
+
+@pytest.mark.xfail
+class TestCreateUserView(LoginRequiredMixin):
+    """Test suite for the CreateUserView."""
+
+    def _get_url(self):
+        return reverse("create_user")
+
+    def test_get(self, client):
+        """Test the view GET request uses the correct template."""
+        with assertTemplateUsed(template_name="main/create_user.html"):
+            response = client.get(self._get_url())
+        assert response.status_code == HTTPStatus.OK
