79f3c1dbb90824832226deefae9ea6e8d1cddc17
Tamora D. James <t.d.james@sheffield.ac.uk>
2025-04-29 11:32:11 +0100

    Add user creation form [#174]

--- main/urls.py
+++ main/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("privacy/", views.privacy, name="privacy"),
     path("accounts/", include("django.contrib.auth.urls")),
+    path("register/", views.register, name="register"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,7 +1,9 @@
 """Views for the main app."""
 
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+
+from .forms import CustomUserCreationForm
 
 
 def index(request: HttpRequest) -> HttpResponse:
@@ -20,3 +22,19 @@ def privacy(request: HttpRequest) -> HttpResponse:
       request: A GET request.
     """
     return render(request=request, template_name="main/privacy.html")
+
+
+def register(request: HttpRequest) -> HttpResponse:
+    """View that renders the user registration form page.
+
+    Args:
+      request: An HTTP request.
+    """
+    if request.method == "POST":
+        user_form = CustomUserCreationForm(data=request.POST)
+        if user_form.is_valid():
+            user_form.save()
+            return redirect("login")
+    else:
+        user_form = CustomUserCreationForm()
+    return render(request, "registration/create_user.html", {"user_form": user_form})
