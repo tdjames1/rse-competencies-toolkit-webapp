fb0cf3628d66e8d888183c9f825eaed09eef7c77
Tamora D. James <t.d.james@sheffield.ac.uk>
2025-05-12 11:48:02 +0100

    Resolve mypy errors in UserCreationForm class [#175]

--- main/forms.py
+++ main/forms.py
@@ -5,16 +5,16 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import User
 
 
-class CustomUserCreationForm(UserCreationForm):
+class CustomUserCreationForm(UserCreationForm[User]):
     """Custom user creation form."""
 
-    class Meta(UserCreationForm.Meta):
+    class Meta(UserCreationForm.Meta):  # type: ignore[name-defined]
         """Form metadata."""
 
         model = User
-        fields = UserCreationForm.Meta.fields  # + ("custom_field",)
+        fields = UserCreationForm.Meta.fields  # type: ignore[attr-defined]
 
-    def save(self, commit: bool = True) -> UserCreationForm:
+    def save(self, commit: bool = True) -> User:
         """Save user."""
         # Save the provided password in hashed format
         user = super().save(commit=False)
